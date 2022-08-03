2c7409d1011d9178c55f931b456ca3a36ce4a9c7
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2022-08-03 17:14:02 +0300

    Version0.2

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
-let counter = 900;
+let counterID = 999;
 const employe0 = {
-    ID:"",
+    ID: 0,
     fullName: "Ghazi Samer",
     department:"Administration",
     level:"Senior",
-    putId: function(){
-        this.ID = ++counter;
+    image: "",
+    putId: function(counter){
+        this.ID = counter +1;
+        ++counterID;
     },
     salary: function (min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
+       return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
       },
     print :function(){
         console.log("Employee name:" + this.firstName);
@@ -18,19 +20,24 @@ const employe0 = {
        
 
 };
+
+employe0.putId(counterID);
+// console.log("    ---" +  employe0.ID) 
 var ghaziName_Salary = employe0.salary(1500, 2000);
  
 
 const employe1 = {
-    ID:"",
+    ID: 0,
     fullName: "Lana Ali",
     department:"Finance	",
     level:"Senior",
-    putId: function(){
-        this.ID = ++counter;
+    image:"" ,
+    putId: function(counter){
+        this.ID = counter +1;
+        ++counterID;
     },
     salary: function (min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
+        return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
       },
     print :function(){
         console.log("Employee name:" + this.firstName);
@@ -39,18 +46,22 @@ const employe1 = {
        
 
 };
+employe1.putId(counterID);
+// console.log("    ---" +  employe1.ID) 
 var lanaName_Salary = employe1.salary(1500, 2000);
 
 const employe2 = {
-    ID:"",
+    ID: 0,
     fullName: "Tamara Ayoub",
     department:"Marketing",
     level:"Senior",
-    putId: function(){
-        this.ID = ++counter;
+    image:"" ,
+    putId: function(counter){
+        this.ID = counter +1;
+        ++counterID;
     },
     salary: function (min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
+        return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
       },
     print :function(){
         console.log("Employee name:" + this.firstName);
@@ -59,18 +70,22 @@ const employe2 = {
        
 
 };
+employe2.putId(counterID);
+// console.log("    ---" +  employe2.ID) 
 var tamaraName_Salary = employe2.salary(1500, 2000);
 
 const employe3 = {
-    ID:"",
+    ID: 0,
     fullName: "Safi Walid",
     department:"Administration",
     level:"Mid-Senior",
-    putId: function(){
-        this.ID = ++counter;
+    image:"" ,
+    putId: function(counter){
+        this.ID = counter +1;
+        ++counterID;
     },
     salary: function (min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
+        return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
       },
     print :function(){
         console.log("Employee name:" + this.firstName);
@@ -79,18 +94,22 @@ const employe3 = {
        
 
 };
+employe3.putId(counterID);
+// console.log("    ---" +  employe3.ID) 
 var safiName_Salary = employe3.salary(1000, 1500);
 
 const employe4 = {
-    ID:"",
+    ID: 0,
     fullName: "Omar Zaid",
     department:"Development",
     level:"Senior",
-    putId: function(){
-        this.ID = ++counter;
+    image:"" ,
+    putId: function(counter){
+        this.ID = counter +1;
+        ++counterID;
     },
     salary: function (min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
+        return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
       },
     print :function(){
         console.log("Employee name:" + this.firstName);
@@ -99,18 +118,22 @@ const employe4 = {
        
 
 };
+employe4.putId(counterID);
+// console.log("    ---" +  employe4.ID) 
 var omarName_Salary = employe4.salary(1500, 2000);
 
 const employe5 = {
-    ID:"",
+    ID: 0,
     fullName: "Rana Saleh",
     department:"Development",
     level:"Junior",
-    putId: function(){
-        this.ID = ++counter;
+    image:"" ,
+    putId: function(counter){
+        this.ID = counter +1;
+        ++counterID;
     },
     salary: function (min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
+        return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
       },
     print :function(){
         console.log("Employee name:" + this.firstName);
@@ -119,40 +142,42 @@ const employe5 = {
        
 
 };
+employe5.putId(counterID);
+// console.log("    ---" +  employe5.ID) 
 var ranaName_Salary = employe5.salary(500, 1000);
 
 const employe6 = {
-    ID:"",
+    ID: 0,
     fullName: "Hadi Ahmad",
     department:"Finance",
     level:"Mid-Senior",
-    putId: function(){
-        this.ID = ++counter;
+    image:"" ,
+    putId: function(counter){
+        this.ID = counter +1;
+        ++counterID;
     },
     salary: function (min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
+        return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
       },
     print :function(){
         console.log("Employee name:" + this.firstName);
         console.log("Employee salary:" + this.salary);
+        console.log("Employee salary:" + this.ID);
+
+        
        }
 };
+var allEmployeId6 = employe6.putId(counterID);
+// console.log("    ---" +  employe6.ID) 
 var hadiName_Salary = employe6.salary(1000, 1500);
 
 
-
-
-
-
-
-
-const allSalary =[ghaziName_Salary, lanaName_Salary, tamaraName_Salary, safiName_Salary, omarName_Salary, ranaName_Salary, hadiName_Salary];
-const allEmployee =[employe0, employe1, employe2, employe3, employe4, employe5, employe6];
+const allSalary = [ghaziName_Salary, lanaName_Salary, tamaraName_Salary, safiName_Salary, omarName_Salary, ranaName_Salary, hadiName_Salary];
+const allEmployee = [employe0, employe1, employe2, employe3, employe4, employe5, employe6];
 
 for(let i=0 ; i<allEmployee.length; i++){
-    allEmployee[i].putId();
-    allEmployee[i].salary();
-    console.log(`Employee name: ${allEmployee[i].fullName} \nEmployee salary: ${allSalary[i]}\n\n`)    
+
+    console.log(`Employee name: ${allEmployee[i].fullName} \nEmployee salary: ${allSalary[i]}`)    
     }
 
 
---
+++ constructor.js
@@ -0,0 +1,46 @@
+
+
+   function salary(min,max){
+    return (Math.floor(Math.random() * (max - min) ) + min) * 0.75;
+
+
+}
+const arr = [];
+function AllEmploye(EmployeeName, Department, EmployeeSalary){
+    this.EmployeeName = EmployeeName;
+    this.Department = Department;
+    this.salary = EmployeeSalary;
+    arr.push(this);
+
+    AllEmploye.prototype.printEmployee = function(){
+        // console.log(`Employee name: ${this.EmployeeName} : Department: ${this.Department} : Salary ${this.EmployeeSalary}JD`)
+        document.write(`<p>Employee name: ${this.EmployeeName} :<br> Department: ${this.Department} :<br> Salary ${this.salary}JD</p>`);
+    }
+
+ 
+    
+} //end the employe function
+
+
+const employee0 = new AllEmploye("Ghazi Samer", "Administration", salary(1500,2000));
+const employee1 = new AllEmploye("Lana Ali", "Finance", salary(1500,2000));
+const employee2 = new AllEmploye("Tamara Ayoub", "Administration", salary(1500,2000));
+const employee3 = new AllEmploye("Safi Walid", "Development", salary(1000,1500));
+const employee4 = new AllEmploye("Rana Saleh", "Development	", salary(500,1000));
+const employee5 = new AllEmploye("Hadi Ahmad", "Finance", salary(1000,1500));
+
+
+employee0.printEmployee();
+employee1.printEmployee();
+employee2.printEmployee();
+employee3.printEmployee();
+employee4.printEmployee();
+employee5.printEmployee();
+
+
+
+
+console.log(arr);
+// console.log(employee0.EmployeeName);
+
+
